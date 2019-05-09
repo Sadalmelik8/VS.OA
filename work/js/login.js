415d1a224fa65d9cb16a951c6b6a1add1f7e8a78
xun.dai <1098606513@qq.com>
2019-05-09 14:56:48 +0800

    1

--- work/js/login.js
+++ work/js/login.js
@@ -13,79 +13,74 @@ function GetCookie(key) {
         }
     }
 }
-var login = function () {
-    var rnd = 18181;
-    var username = document.getElementById('nm').value;
-    var s = "svr=WS_00001&rnd=" + rnd + "&mobile=" + username;
-    var URL = "/webservice/?" + s;
-    $.ajax({
-        type: 'get',
-        url: URL,
-        datatype: 'json',
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function (msg) {
-            try {
-                if (msg.status == "ok") {
-                    document.cookie = 'wytSession=' + msg.ret.fsession;
-                }
-            }
-            catch (e) { }
-        }
-    })
-}
-var logins = function () {
-    //获取fsession
-    var aCookie = GetCookie('wytSession');
-    session = eval('(' + aCookie + ')');
-    if (session) {
-        if (session.fsession == "undefined") {
-            window.open('login.html', '_self');
-            return;
-        }
-    }
-    else {
-        window.open('login.html', '_self');
-        return;
-    }
-    var fsession = session.fsession;
-    tohtml = "index.html";
-    var username = document.getElementById('nm').value;
-    var password = document.getElementById('pw').value;
-    var s = "svr=WS_00000&mobile=" + username + "&smscode=" + password + "&fsession=" + fsession;
-    var URL = "/webservice/?" + s;
-    $.ajax({
-        type: 'get',
-        url: URL,
-        datatype: 'json',
-        cache: false,
-        processData: false,
-        contentType: false,
-        success: function (msg) {
-            try {
-                if (msg.status == "ok") {
-                    var aCookie = '{\'ORG_NM\':\'' + msg.ret.username;
-                    aCookie += '\',\'fsession\':\'' + msg.ret.session;
-                    aCookie += '}';
-                    var expdate = new Date();
-                    expdate.setTime(expdate.getTime() + (86400 * 1000 * 1));
-                    document.cookie = 'wytSession=' + escape(aCookie)
-                        + ';expires=' + expdate.toGMTString();
-                    window.open(tohtml, '_self');
-                }
-            }
-            catch (e) { }
-        }
-    })
-}
 $(document).ready(function () {
     var tohtml = "";
     $('#session').click(function () {
-        login();
+        Math.floor(Math.random() * 10);
+        var rnd;
+        var username = document.getElementById('nm').value;
+        var s = "svr=WS_00001&rnd=" + rnd + "&mobile=" + username;
+        var URL = "/webservice/?" + s;
+        $.ajax({
+            type: 'get',
+            url: URL,
+            datatype: 'json',
+            cache: false,
+            processData: false,
+            contentType: false,
+            success: function (msg) {
+                try {
+                    if (msg.status == "ok") {
+                        document.cookie = 'wytSession=' + msg.ret.fsession;
+                    }
+                }
+                catch (e) { }
+            }
+        })
     })
     $('#dl').click(function () {
-        logins();
+        //获取fsession
+        var aCookie = GetCookie('wytSession');
+        session = eval('(' + aCookie + ')');
+        if (session) {
+            if (session.fsession == "undefined") {
+                window.open('login.html', '_self');
+                return;
+            }
+        }
+        else {
+            window.open('login.html', '_self');
+            return;
+        }
+        var fsession = session.fsession;
+        tohtml = "index.html";
+        var username = document.getElementById('nm').value;
+        var password = document.getElementById('pw').value;
+        var s = "svr=WS_00000&mobile=" + username + "&smscode=" + password + "&fsession=" + fsession;
+        var URL = "/webservice/?" + s;
+        $.ajax({
+            type: 'get',
+            url: URL,
+            datatype: 'json',
+            cache: false,
+            processData: false,
+            contentType: false,
+            success: function (msg) {
+                try {
+                    if (msg.status == "ok") {
+                        var aCookie = '{\'ORG_NM\':\'' + msg.ret.username;
+                        aCookie += '\',\'fsession\':\'' + msg.ret.session;
+                        aCookie += '}';
+                        var expdate = new Date();
+                        expdate.setTime(expdate.getTime() + (86400 * 1000 * 1));
+                        document.cookie = 'wytSession=' + escape(aCookie)
+                            + ';expires=' + expdate.toGMTString();
+                        window.open(tohtml, '_self');
+                    }
+                }
+                catch (e) { }
+            }
+        })
     })
 })
 
