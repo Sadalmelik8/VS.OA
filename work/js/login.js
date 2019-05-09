1fb8b24f52c08466b4fd3ea8e906f21f22d43fa9
xun.dai <1098606513@qq.com>
2019-05-09 14:38:30 +0800

    login.js

--- work/js/login.js
+++ work/js/login.js
@@ -3,14 +3,21 @@
         login();
     }
 }
+//获取cookkie
+function GetCookie(key) {
+    var aCookie = document.cookie.split("; ");
+    for (var i = 0; i < aCookie.length; i++) {
+        var aCrumb = aCookie[i].split("=");
+        if (key == aCrumb[0]) {
+            return unescape(aCrumb[1]);
+        }
+    }
+}
 var login = function () {
-    tohtml = "index.html";
-    document.cookie = 'wytSession=;';
+    var rnd = 18181;
     var username = document.getElementById('nm').value;
-    var password = document.getElementById('pw').value;
-    var t = new Date().getTime();
-    var s = "svr=WS_00000&userNum=" + username + "&pd=" + password + "&t=" + t;
-    var URL = "/webadmin/?" + s;
+    var s = "svr=WS_00001&rnd=" + rnd + "&mobile=" + username;
+    var URL = "/webservice/?" + s;
     $.ajax({
         type: 'get',
         url: URL,
@@ -21,23 +28,45 @@ var login = function () {
         success: function (msg) {
             try {
                 if (msg.status == "ok") {
-                    var aCookie = '{\'ORG_NM\':\'' + msg.ret.user.nm;
-                    aCookie += '\',\'ORG_NO\':\'' + msg.ret.user.id
-                        + '\',\'fsession\':\'' + msg.ret.session
-                        + '\',\'ORG_NO\':\'' + msg.ORG_NO + '\',\'UI_STYLE\':\''
-                        + msg.UI_STYLE + '\',\'OPER_ID\':\'' + msg.OPER_ID
-                        + '\',\'LOGIN_TM\':\'' + msg.LOGIN_TM + '\',\'SYS_CNL\':\''
-                        + msg.SYS_CNL + '\',\'CUR_OPER_NO\':\'' + msg.CUR_OPER_NO
-                        + '\',\'OPER_PROP\':\'' + msg.OPER_PROP
-                        + '\',\'IS_SCREEN\':\'' + msg.IS_SCREEN
-                        + '\',\'IS_PAGE\':\'' + msg.IS_PAGE
-                        + '\',\'MS_REFRESH\':\'' + msg.MS_REFRESH
-                        + '\',\'LINE_SHOW\':\'' + msg.LINE_SHOW
-                        + '\',\'LAST_LOG_TM\':\'' + msg.LAST_LOG_TM
-                        + '\',\'UPD_OPER\':\'' + msg.UPD_OPER + '\',\'LOG_MOD\':\''
-                        + msg.LOG_MOD + '\',\'OPER_NM\':\'' + msg.ret.user.nm
-                        + '\',\'OPER_NO\':\'' + msg.ret.user.id + '\',';
-                    aCookie += '\'keep_NM\':\'0\'';
+                    document.cookie = 'wytSession=' + msg.ret.fsession;
+                }
+            }
+            catch (e) { }
+        }
+    })
+}
+var logins = function () {
+    //获取fsession
+    var aCookie = GetCookie('wytSession');
+    session = eval('(' + aCookie + ')');
+    if (session) {
+        if (session.fsession == "undefined") {
+            window.open('login.html', '_self');
+            return;
+        }
+    }
+    else {
+        window.open('login.html', '_self');
+        return;
+    }
+    var fsession = session.fsession;
+    tohtml = "index.html";
+    var username = document.getElementById('nm').value;
+    var password = document.getElementById('pw').value;
+    var s = "svr=WS_00000&mobile=" + username + "&smscode=" + password + "&fsession=" + fsession;
+    var URL = "/webservice/?" + s;
+    $.ajax({
+        type: 'get',
+        url: URL,
+        datatype: 'json',
+        cache: false,
+        processData: false,
+        contentType: false,
+        success: function (msg) {
+            try {
+                if (msg.status == "ok") {
+                    var aCookie = '{\'ORG_NM\':\'' + msg.ret.username;
+                    aCookie += '\',\'fsession\':\'' + msg.ret.session;
                     aCookie += '}';
                     var expdate = new Date();
                     expdate.setTime(expdate.getTime() + (86400 * 1000 * 1));
@@ -52,8 +81,11 @@ var login = function () {
 }
 $(document).ready(function () {
     var tohtml = "";
-    $('#dl').click(function () {
+    $('#session').click(function () {
         login();
+    })
+    $('#dl').click(function () {
+        logins();
     })
 })
 
