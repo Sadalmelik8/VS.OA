2d22501e0376b8f824ab8e7981897ef99a501114
xun.dai <1098606513@qq.com>
2019-05-14 16:59:27 +0800

    1

--- work/js/index-js.js
+++ work/js/index-js.js
@@ -47,7 +47,7 @@ $(document).ready(function () {
     var fsession = session.fsession;
     var userName = session.User_NM;
     _template1 = buildJson();
-    var s = ("svr=WS_000009" + "&fsession=" + fsession + "&userName=" + userName);
+    var s = ("svr=WS_00009" + "&fsession=" + fsession + "&userName=" + userName);
     var URL = "/webservice/?" + s;
     var form = new FormData();
     form.append("data", (JSON.stringify(_template1)));
