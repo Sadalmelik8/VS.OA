0696ed0ec5c3f1136e11408ab45deff4f5f2298f
xun.dai <1098606513@qq.com>
2019-05-17 10:14:23 +0800

    1

--- work/js/update.js
+++ work/js/update.js
@@ -61,8 +61,8 @@ $(document).ready(function () {
             remarks.value = dataObj.ret[0].remarks;
             level.value = dataObj.ret[0].level;
             detailed.valve = dataObj.ret[0].proposetime + dataObj.ret[0].content;
-            $.each(dataObj.ret, function (index, item) {
-                con += "<li><span>" + item.contents.submittime + "</span>" + "<span>" + item.contents.contents + "</span></li>";
+            $.each(dataObj.ret[1].contents, function (index, item) {
+                con += "<li><span>" + item.submittime + "</span>" + "<span>" + item.contents + "</span></li>";
                 $("#detalis").html(con); //把内容入到这个div中
             })
         }
