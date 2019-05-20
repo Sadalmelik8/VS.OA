97d875ec533e72a705f1959a9fbf7f3a17bca475
xun.dai <1098606513@qq.com>
2019-05-20 14:27:33 +0800

    1

--- work/js/submit.js
+++ work/js/submit.js
@@ -23,54 +23,50 @@
             window.open('login.html', '_self');
             return;
         }
-        _template1 = buildJson();
-        var fsession = session.fsession;
-        var userName = session.User_NM;
-        var s = ("svr=WS_00002" + "&fsession=" + fsession + "&userName=" + userName);
-        var URL = "/webservice/?" + s;
-        var form = new FormData();
-        form.append("data", (JSON.stringify(_template1)));
-        $.ajax({
-            type: 'post',
-            url: URL,
-            contentType: "application/json",//如果想以json格式把数据提交到后台的话，这个必须有，否则只会当做表单提交
-            data: form,
-            cache: false,
-            processData: false,
-            contentType: false,
-            dataType: "json",//期待返回的数据类型
-            success: function (msg) {
-                if (msg.ret[0].id == '1') {
-                    alert('上传成功');
-                }
-                else {
-                    alert('上传失败');
-                }
-            },
-            error: function () {
-                alert("请求失败");
-            }
-        });
-    });
-    function buildJson() {
-        let title = document.getElementsByClassName('title')[0].value;
-        let particular = document.getElementById('icon').value;
-        var std = JSON.stringify({});
-        var stdTemplate = JSON.parse(std);
-        stdTemplate.title = title;
-        stdTemplate.content = particular;
-        stdTemplate.pic = file();
-        return stdTemplate;
-    }
-    function file() {
         for (var i = 0; i < $("#file")[0].files.length; i++) {
             var reader = new FileReader();
             reader.readAsDataURL($("#file")[0].files[i]);
             reader.onload = function (e) {
                 var data = '';
                 data += e.target.result + '!@#$%^&*';
-                return data;
+                buildJson(data);
             }
         }
-    }
+        var fsession = session.fsession;
+        var userName = session.User_NM;
+        var s = ("svr=WS_00002" + "&fsession=" + fsession + "&userName=" + userName);
+        var URL = "/webservice/?" + s;
+        var form = new FormData();
+        function buildJson(data) {
+            let title = document.getElementsByClassName('title')[0].value;
+            let particular = document.getElementById('icon').value;
+            var std = JSON.stringify({});
+            var stdTemplate = JSON.parse(std);
+            stdTemplate.title = title;
+            stdTemplate.content = particular;
+            stdTemplate.pic = data;
+            form.append("data", (JSON.stringify(stdTemplate)));
+            $.ajax({
+                type: 'post',
+                url: URL,
+                contentType: "application/json",//如果想以json格式把数据提交到后台的话，这个必须有，否则只会当做表单提交
+                data: form,
+                cache: false,
+                processData: false,
+                contentType: false,
+                dataType: "json",//期待返回的数据类型
+                success: function (msg) {
+                    if (msg.ret[0].id == '1') {
+                        alert('上传成功');
+                    }
+                    else {
+                        alert('上传失败');
+                    }
+                },
+                error: function () {
+                    alert("请求失败");
+                }
+            });
+        }
+    });
 });
