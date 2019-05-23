032ec0d0dbc9f33e14bcc6a30ddd422178d0345d
xun.dai <1098606513@qq.com>
2019-05-23 10:14:23 +0800

    1

--- work/js/index.js
+++ work/js/index.js
@@ -342,6 +342,10 @@ $(document).ready(function () {
 //安全退出
 $(document).ready(function () {
     $('#reset').click(function () {
-        document.cookie = 'wytSession=' + '';
+        var keys = document.cookie.match(/[^ =;]+(?=\=)/g);
+        if (keys) {
+            for (var i = keys.length; i--;)
+                document.cookie = keys[i] + '=0;expires=' + new Date(0).toUTCString()
+        }
     })
 })
