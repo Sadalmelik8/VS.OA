60b4f03ce8c9edf23fe51183f1adb2047680fef0
lei.jin <1047316586@qq.com>
2019-05-24 17:16:57 +0800

    1

--- work/js/update-re.js
+++ work/js/update-re.js
@@ -51,12 +51,12 @@ $(document).ready(function () {
     var con = '';
     var cons = '';
     var title = document.getElementsByClassName('title')[0];
-    var presents = document.getElementsByClassName('present')[0];
+    // var presents = document.getElementsByClassName('present')[0];
     var contents = document.getElementsByClassName('contents')[0];
     var remarks = document.getElementsByClassName('title')[1];
-    var level = document.getElementsByClassName('bottom--middle__urgency')[0];
+    // var level = document.getElementsByClassName('bottom--middle__urgency')[0];
     _template1 = buildJson();
-    var s = ("svr=WS_00006" + "&fsession=" + fsession + "&userName=" + userName);
+    var s = ("svr=WS_00013" + "&fsession=" + fsession + "&userName=" + userName);
     var URL = "/webservice/?" + s;
     var form = new FormData();
     form.append("data", (JSON.stringify(_template1)));
@@ -72,7 +72,7 @@ $(document).ready(function () {
         success: function (result) {
             var dataObj = result; //返回的result为json格式的数据
             title.value = dataObj.ret[0].title;
-            presents.value = dataObj.ret[0].executor;
+            // presents.value = dataObj.ret[0].executor;
             remarks.value = dataObj.ret[0].remarks;
             if (dataObj.ret[0].level == null) {
                 level.value = 1;
@@ -152,93 +152,7 @@ $(document).ready(function () {
         stdTemplate.sts = 1;
         return stdTemplate;
     }
-    $('#present').click(function () {
-        var fsession = session.fsession;
-        var userName = session.User_NM;
-        var s = ("svr=WS_00007" + "&fsession=" + fsession + "&userName=" + userName);
-        var URL = "/webservice/?" + s;
-        var form = new FormData();
-        if ($("#file")[0].files.length > 0) {
-            for (var i = 0; i < $("#file")[0].files.length; i++) {
-                var reader = new FileReader();
-                reader.readAsDataURL($("#file")[0].files[i]);
-                reader.onload = function (e) {
-                    var data = '';
-                    data += e.target.result + '!@#$%^&*';
-                    buildJson(data);
-                }
-            }
-        }
-        else {
-            var data = '';
-            buildJson(data);
-        }
-        function buildJson(data) {
-            var std = JSON.stringify({});
-            var stdTemplate = JSON.parse(std);
-            stdTemplate.executor = presents.value;
-            stdTemplate.num = oltid;
-            stdTemplate.contents = contents.value;
-            stdTemplate.remarks = remarks.value;
-            stdTemplate.level = level.value;
-            stdTemplate.pic = data;
-            form.append("data", (JSON.stringify(stdTemplate)));
-            $.ajax({
-                type: 'post',
-                url: URL,
-                contentType: "application/json",//如果想以json格式把数据提交到后台的话，这个必须有，否则只会当做表单提交
-                data: form,
-                cache: false,
-                processData: false,
-                contentType: false,
-                dataType: "json",//期待返回的数据类型
-                success: function (msg) {
-                    if (msg.ret.id == '0') {
-                        alert('上传失败');
-                    }
-                    else {
-                        alert('上传成功');
-                    }
-                },
-                error: function () {
-                    alert("请求失败");
-                }
-            });
-        }
-    });
-    $('#problemState').click(function () {
-        var fsession = session.fsession;
-        var userName = session.User_NM;
-        _template1 = buildJson();
-        var s = ("svr=WS_00008" + "&fsession=" + fsession + "&userName=" + userName);
-        var URL = "/webservice/?" + s;
-        var form = new FormData();
-        form.append("data", (JSON.stringify(_template1)));
-        $.ajax({
-            type: "post", //请求的方式，也有get请求
-            url: URL, //请求地址，后台提供的,这里我在//本地自己建立了个json的文件做例子
-            contentType: "application/json",
-            data: form,//data是传给后台的字段，后台需要哪些就传入哪些
-            cache: false,
-            processData: false,
-            contentType: false,
-            dataType: "json", //json格式，后台返回的数据为json格式的。
-            success: function (result) {
-                dataObj = result;
-                if (dataObj.ret.id == 0) {
-                    alert('失败');
-                } else {
-                    alert('成功');
-                }
-            }
-        });
-        function buildJson() {
-            var std = JSON.stringify({});
-            var stdTemplate = JSON.parse(std);
-            stdTemplate.num = oltid;
-            return stdTemplate;
-        }
-    });
+
     $("#big").click(function () {
         var big = document.getElementById('big');
         big.src = '';
