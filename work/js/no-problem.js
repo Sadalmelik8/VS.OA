f18499da4d1b221ea8de9718bdf490af79f72744
lei.jin <1047316586@qq.com>
2019-06-04 11:28:21 +0800

    1

--- work/js/no-problem.js
+++ work/js/no-problem.js
@@ -1031,9 +1031,12 @@ $(document).ready(function () {
             return stdTemplate;
         }
     });
+});
+$(document).ready(function () {
     document.getElementById('ul').addEventListener('click', function (e) {
         this.childNodes.forEach(function (v, k) {
             if (e.target === document.getElementsByClassName('knowledge0')[k]) {
+                console.log(1);
                 var fsession = session.fsession;
                 var userName = session.User_NM;
                 _template1 = buildJson();
