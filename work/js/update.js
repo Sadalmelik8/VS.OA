f0cce0c93ed26357fe645ffe7799f717f2f8915b
lei.jin <1047316586@qq.com>
2019-06-21 16:38:43 +0800

    1

--- work/js/update.js
+++ work/js/update.js
@@ -92,8 +92,10 @@ let _click = function(){
 
     }
 };
-$("#click").click(function () {
-    _click();
+$(document).ready(function () {
+    $("#click").click(function () {
+        _click();
+    });
 });
 document.addEventListener('paste', function (event) {
     var items = (event.clipboardData || window.clipboardData).items;
@@ -332,154 +334,108 @@ $(document).ready(function () {
         return stdTemplate;
     }
     $('#present').click(function () {
-        //var fsession = session.fsession;
-        //var userName = session.User_NM;
-        //var s = ("svr=WS_00007" + "&fsession=" + fsession + "&userName=" + userName);
-        //var URL = "/webservice/?" + s;
-        //var form = new FormData();
-        //if ($("#file")[0].files.length > 0) {
-        //    for (var i = 0; i < $("#file")[0].files.length; i++) {
-        //        var reader = new FileReader();
-        //        reader.readAsDataURL($("#file")[0].files[i]);
-        //        reader.onload = function (e) {
-        //            var data = '';
-        //            data += e.target.result + '!@#$%^&*';
-        //            buildJson(data);
-        //        }
-        //    }
-        //}
-        //else {
-        //    var data = '';
-        //    buildJson(data);
-        //}
-        //function buildJson(data) {
-        //    var std = JSON.stringify({});
-        //    var stdTemplate = JSON.parse(std);
-        //    stdTemplate.executor = presents.value;
-        //    stdTemplate.num = oltid;
-        //    stdTemplate.contents = contents.value;
-        //    stdTemplate.remarks = remarks.value;
-        //    stdTemplate.level = level.value;
-        //    stdTemplate.pic = data;
-        //    form.append("data", (JSON.stringify(stdTemplate)));
-        //    $.ajax({
-        //        type: 'post',
-        //        url: URL,
-        //        contentType: "application/json",//如果想以json格式把数据提交到后台的话，这个必须有，否则只会当做表单提交
-        //        data: form,
-        //        cache: false,
-        //        processData: false,
-        //        contentType: false,
-        //        dataType: "json",//期待返回的数据类型
-        //        success: function (msg) {
-        //            if (msg.ret.id == '0') {
-        //                alert('上传失败');
-        //            }
-        //            else {
-        //                alert('上传成功');
-        //            }
-        //        },
-        //        error: function () {
-        //            alert("请求失败");
-        //        }
-        //    });
-        //}
         let presents = document.getElementById("presents");
         let click = document.getElementById("click");
-        if (presents.value == ''){
+        if (presents.value == '') {
             alert("请选择受理人")
-        }if (click.innerHTML === '请选择问题类别'){
-            alert("请选择问题类别")
-        } else {
-            var fsession = session.fsession;
-            var userName = session.User_NM;
-            var s = ("svr=WS_00007" + "&fsession=" + fsession + "&userName=" + userName);
-            var URL = "/webservice/?" + s;
-            var form = new FormData();
-            for (var i = 0; i < 6; i++) {
-                var img = document.getElementsByClassName('imgs')[i].src;
-                if (img != document.location.href) {
-                    var file = '';
-                    buildJson(file, img);
-                }
+        }
+        else {
+            if (click.innerHTML === '请选择问题类别') {
+                alert("请选择问题类别")
             }
-            $("#file").each(function () {
-                if ($("#file")[0].files.length > 0) {
-                    for (var i = 0; i < $("#file")[0].files.length; i++) {
-                        var file = $("#file")[0].files[i];
-                        var img = '';
+            else {
+                var fsession = session.fsession;
+                var userName = session.User_NM;
+                var s = ("svr=WS_00007" + "&fsession=" + fsession + "&userName=" + userName);
+                var URL = "/webservice/?" + s;
+                var form = new FormData();
+                for (var i = 0; i < 6; i++) {
+                    var img = document.getElementsByClassName('imgs')[i].src;
+                    if (img != document.location.href) {
+                        var file = '';
                         buildJson(file, img);
                     }
                 }
-                else {
-                    var file = "";
-                    var img = '';
-                    buildJson(file, img);
-                }
-            });
-            $.ajax({
-                type: "POST",
-                url: URL,
-                data: form,
-                dataType: "json",
-                cache: false,
-                processData: false,
-                contentType: false,
-                success: function (msg) {
-                    if (msg.ret.id == '1') {
-                        alert('上传成功');
-                        window.open('problem.html', '_parent');
-                    } else {
-                        alert('上传失败');
+                $("#file").each(function () {
+                    if ($("#file")[0].files.length > 0) {
+                        for (var i = 0; i < $("#file")[0].files.length; i++) {
+                            var file = $("#file")[0].files[i];
+                            var img = '';
+                            buildJson(file, img);
+                        }
                     }
-                },
-                error: function () {
-                    alert("请求失败");
-                }
-            });
-            function buildJson(file, img) {
-                var std = JSON.stringify({});
-                var stdTemplate = JSON.parse(std);
-                stdTemplate.executor = presents.value;//受理人
-                stdTemplate.num = oltid;//num
-                stdTemplate.contents = contents.value;//追加内容
-                stdTemplate.remarks = remarks.value;//备注
-                stdTemplate.level = level.value;//紧急度
-                stdTemplate.category = click.innerHTML;//问题类别
-                if (form.get("data") != null) {
-                    if (file == "" && img != "") {
-                        form.append("pic", img);
+                    else {
+                        var file = "";
+                        var img = '';
+                        buildJson(file, img);
                     }
-                    if (img == "" && file != "") {
-                        form.append("files", file);
+                });
+                $.ajax({
+                    type: "POST",
+                    url: URL,
+                    data: form,
+                    dataType: "json",
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                    success: function (msg) {
+                        if (msg.ret.id == '1') {
+                            alert('上传成功');
+                            window.open('problem.html', '_parent');
+                        } else {
+                            alert('上传失败');
+                        }
+                    },
+                    error: function () {
+                        alert("请求失败");
                     }
-                    if (file != "" && img != "") {
-                        form.append("pic", img);
-                        form.append("files", file);
+                });
+                function buildJson(file, img) {
+                    var std = JSON.stringify({});
+                    var stdTemplate = JSON.parse(std);
+                    stdTemplate.executor = presents.value;//受理人
+                    stdTemplate.num = oltid;//num
+                    stdTemplate.contents = contents.value;//追加内容
+                    stdTemplate.remarks = remarks.value;//备注
+                    stdTemplate.level = level.value;//紧急度
+                    stdTemplate.category = click.innerHTML;//问题类别
+                    if (form.get("data") != null) {
+                        if (file == "" && img != "") {
+                            form.append("pic", img);
+                        }
+                        if (img == "" && file != "") {
+                            form.append("files", file);
+                        }
+                        if (file != "" && img != "") {
+                            form.append("pic", img);
+                            form.append("files", file);
+                        }
+                        if (form.get("files") == null) {
+                            form.append("files", file);
+                        }
+                        if ((form.get("pic") == null)) {
+                            form.append("pic", img);
+                        }
                     }
-                    if (form.get("files") == null) {
-                        form.append("files", file);
-                    }
-                    if ((form.get("pic") == null)) {
-                        form.append("pic", img);
-                    }
-                }
-                else {
-                    form.append("data", (JSON.stringify(stdTemplate)));
-                    if (file == "" && img != "") {
-                        form.append("pic", img);
-                    }
-                    if (img == "" && file != "") {
-                        form.append("files", file);
-                    }
-                    if (file != "" && img != "") {
-                        form.append("pic", img);
-                        form.append("files", file);
+                    else {
+                        form.append("data", (JSON.stringify(stdTemplate)));
+                        if (file == "" && img != "") {
+                            form.append("pic", img);
+                        }
+                        if (img == "" && file != "") {
+                            form.append("files", file);
+                        }
+                        if (file != "" && img != "") {
+                            form.append("pic", img);
+                            form.append("files", file);
+                        }
                     }
                 }
             }
         }
+
     });
+
     $('#problemState').click(function () {
         var fsession = session.fsession;
         var userName = session.User_NM;
