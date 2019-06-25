4903eca5e0195a259411a809031b069f75d0c7ff
lei.jin <1047316586@qq.com>
2019-06-25 10:44:15 +0800

    1

--- work/js/no-problem.js
+++ work/js/no-problem.js
@@ -57,7 +57,7 @@ let _click = function(){
             $.each(dataObj.ret[1], function (index, item) {
                 con += "<span class='system'>" + item + "</span>";
             });
-            con+= "<span class='system' id='cancel'>" +"取消"+ "</span>";
+            con+= "<span class='system' id='cancel1'>" +"取消"+ "</span>";
             $('.category').html(con);
 
             $(".system").click(function (e) {
@@ -72,11 +72,11 @@ let _click = function(){
                     _click();
                 });
                 $(".subclass").click(function (e) {
+                    let coned = e.target.innerText;
                     if (e.target.innerHTML == '返回') {
                         document.getElementById("click").innerHTML = "请选择问题类别";
                     }else {
                         document.getElementById("click").innerHTML = e.target.innerHTML;
-                        document.getElementsByClassName("category")[0].style.display = 'none';
                         //获取cookkie
                         function GetCookie(key) {
                             var aCookie = document.cookie.split("; ");
@@ -113,35 +113,42 @@ let _click = function(){
                             type: "post", //请求的方式，也有get请求
                             url: URL, //请求地址，后台提供的,这里我在//本地自己建立了个json的文件做例子
                             contentType: "application/json",
-                            data: {},//data是传给后台的字段，后台需要哪些就传入哪些
+                            data: form,//data是传给后台的字段，后台需要哪些就传入哪些
                             cache: false,
                             processData: false,
                             contentType: false,
                             dataType: "json", //json格式，后台返回的数据为json格式的。
                             success: function (result) {
                                 dataObj = result;
-                                $.each(dataObj.ret.username, function (indexs, item) {
+                                $.each(dataObj.ret, function (indexs, item) {
                                     con += "<li class='datas'>"
                                         + "<span class='number'>" + (indexs + 1) + "</span>"//序号
                                         + "<span class='caption' id=" + item.num + ">" + item.title + "</span>"//标题
-                                        + "<span>" + item.category + "</span>"//问题类别
+                                        + "<span class='sort'>" + item.category + "</span>"//问题类别
                                         + "<span>" + item.introducer + "</span>"//提出人
                                         + "<span class='time'>" + item.proposetime.substr(0, 4) + '/' + item.proposetime.substr(4, 2) + '/' + item.proposetime.substr(6, 2) + '&nbsp;' + item.proposetime.substr(8, 2) + ':' + item.proposetime.substr(10, 2) + ':' + item.proposetime.substr(12, 2) + "</span>"//提出时间
                                         + "<span>" + item.executor + "</span>"//受理人
                                         + "<span class='time'>" + item.submittime.substr(0, 4) + '/' + item.submittime.substr(4, 2) + '/' + item.submittime.substr(6, 2) + '&nbsp;' + item.submittime.substr(8, 2) + ':' + item.submittime.substr(10, 2) + ':' + item.submittime.substr(12, 2) + "</span>"//提交时间
-                                        + "<span class='time'>"+  item.acceptor + "&nbsp;" + item.inspecttime.substr(0, 4) + '/' + item.inspecttime.substr(4, 2) + '/' + item.inspecttime.substr(6, 2) + '&nbsp;' + "</span>"//验收
+                                        + "<span class='time1'>"+  item.acceptor + "&nbsp;" + item.inspecttime.substr(0, 4) + '/' + item.inspecttime.substr(4, 2) + '/' + item.inspecttime.substr(6, 2) + '&nbsp;' + "</span>"//验收
                                         + "<input type='submit' class=" + "knowledge" + item.knowledge + ">"
                                         + "</li>";
                                     $("#ul").html(con); //把内容入到这个div中
+                                    actived();
+                                    redirect();
+                                    paginationed();
+                                    document.getElementsByClassName("subclass").style.display = 'none';
+                                    document.getElementById("back").style.display = "none"
                                 })
                             }
                         });
                         function buildJson() {
                             var std = JSON.stringify({});
                             var stdTemplate = JSON.parse(std);
-                            stdTemplate.category = e.target.innerHTML;
+                            stdTemplate.category = coned;
+                            stdTemplate.problemstate = 0;
                             return stdTemplate;
                         }
+                        document.getElementsByClassName("category")[0].style.display = 'none';
                     }
                 });
             });
