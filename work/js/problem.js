8672e772a463a07134b5e55a83f2d118a7b63c71
lei.jin <1047316586@qq.com>
2019-06-25 17:10:30 +0800

    1

--- work/js/problem.js
+++ work/js/problem.js
@@ -123,15 +123,15 @@ let _click = function(){
                             success: function (result) {
                                 dataObj = result;
                                 $.each(dataObj.ret, function (indexs, item) {
-                                    // if (dataObj.ret.id == 0){
-                                    //     alert('该类别下没有相关问题')
-                                    // } else {
+                                    if (dataObj.ret[0].id == 0){
+                                        alert('该类别下没有相关问题')
+                                    } else {
                                         paginationed();
                                         actived();
                                         redirect();
                                         document.getElementsByClassName("subclass").style.display = 'none';
                                         document.getElementById("back").style.display = "none"
-                                    // }
+                                    }
                                     // con += "<li class='datas'>"
                                     //     + "<span class='number'>" + (indexs + 1) + "</span>"//序号
                                     //     + "<span class='caption' id=" + item.num + ">" + item.title + "</span>"//标题
