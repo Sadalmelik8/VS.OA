503b080ea473b963ea212135d83b6f791bd83629
lei.jin <1047316586@qq.com>
2019-06-26 19:18:59 +0800

    1

--- work/js/repository.js
+++ work/js/repository.js
@@ -8,15 +8,15 @@ function GetCookie(key) {
         }
     }
 }
-function stopBubble(e) {
-    //如果提供了事件对象，则这是一个非IE浏览器
-    if (e && e.stopPropagation)
-    //因此它支持W3C的stopPropagation()方法
-        e.stopPropagation();
-    else
-    //否则，我们需要使用IE的方式来取消事件冒泡
-        window.event.cancelBubble = true;
-}
+// function stopBubble(e) {
+//     //如果提供了事件对象，则这是一个非IE浏览器
+//     if (e && e.stopPropagation)
+//     //因此它支持W3C的stopPropagation()方法
+//         e.stopPropagation();
+//     else
+//     //否则，我们需要使用IE的方式来取消事件冒泡
+//         window.event.cancelBubble = true;
+// }
 window.onload = function () {
     call()
 };
