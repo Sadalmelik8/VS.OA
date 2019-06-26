d35c93e0dae583634193078231951914f07ea636
lei.jin <1047316586@qq.com>
2019-06-26 11:11:04 +0800

    1

--- service/js/log.js
+++ service/js/log.js
@@ -234,74 +234,113 @@ $(document).ready(function () {
     });
     $('#search').click(function () {
         let input = document.getElementsByClassName("log--header__left")[0].value;
-        var time = new Date();
-        var day = time.getDate();
-        var month = time.getMonth() + 1;
-        var year = time.getFullYear();
-        if (month < 10 && day < 10) {
-            var today = time.getFullYear() + "-" + ('0' + month) + "-" + ('0' + day);
-            $('#data').val(today);
+        // var seek = document.getElementById("seek").value;
+        var aCookie = GetCookie('wytSession');
+        session = eval('(' + aCookie + ')');
+        if (session) {
+            if (session.fsession == "undefined") {
+                window.open('login.html', '_self');
+                return;
+            }
         }
-        if (month >= 10 && day >= 10) {
-            var today = time.getFullYear() + "-" + (month) + "-" + (day);
-            $('#data').val(today);
+        else {
+            window.open('login.html', '_self');
+            return;
         }
-        if (month >= 10 && day < 10) {
-            var today = time.getFullYear() + "-" + (month) + "-" + ('0' + day);
-            $('#data').val(today);
-        }
-        if (month < 10 && day >= 10) {
-            var today = time.getFullYear() + "-" + ('0' + month) + "-" + (day);
-            $('#data').val(today);
-        }
-        _template1 = buildJson();
-        var s = ("svr=webadmin_00003" + "&fsession=" + fsession);
+        var fsession = session.fsession;
+        var s = ("svr=webadmin_00005" + "&fsession=" + fsession);
         var URL = "/webadmin/?" + s;
-        var form = new FormData();
-        form.append("data", (JSON.stringify(_template1)));
-        var arr = [];
-        var arrs = [];
         $.ajax({
-            type: "post", //请求的方式，也有get请求
-            url: URL, //请求地址，后台提供的,这里我在本地自己建立了个json的文件做例子
-            data: form,//data是传给后台的字段，后台需要哪些就传入哪些
+            type: "get", //请求的方式，也有get请求
+            url: URL,
+            data: {},//data是传给后台的字段，后台需要哪些就传入哪些
             dataType: "json", //json格式，后台返回的数据为json格式的。
-            cache: false,
-            processData: false,
-            contentType: false,
+            //beforeSend: LoadFunction, //加载执行方法
+            //错误执行方法
             error: function (XMLHttpRequest, textStatus, errorThrown) {
                 alert(XMLHttpRequest.status);
+                alert(XMLHttpRequest.readyState);
+                alert(textStatus);
+                alert(errorThrown);
             },
             success: function (result) {
-                var dataObj = result;
-                if (dataObj.ret.ls.length == 0) {
-                    alert('当天没有服务日志');
-                }
-                var con = '';
-                document.getElementById('code').value = dataObj.ret.content;
-                $.each(dataObj.ret.ls, function (indexs, item) {
-                    arr.push(item);
-                });
-                for (var i = arr.length - 1; i >= 0; i--) {
-                    con += "<li>"
-                        + "<span class='log--icon__time' id=" + arr[i].nm + ">" + arr[i].m + "</span>"
-                        + "</li>";
-                }
-                $("#ul").html(con);
-                $("span").click(function () {
-                    logs(this.id);
+                dataObj = result;//返回的result为json格式的数据
+                $.each(dataObj.ret, function (index, item) {
+                    if (item.svr != seek && dataObj.ret.length == (index + 1)) {
+                        alert("无此服务");
+                    }else {
+                        var time = new Date();
+                        var day = time.getDate();
+                        var month = time.getMonth() + 1;
+                        var year = time.getFullYear();
+                        if (month < 10 && day < 10) {
+                            var today = time.getFullYear() + "-" + ('0' + month) + "-" + ('0' + day);
+                            $('#data').val(today);
+                        }
+                        if (month >= 10 && day >= 10) {
+                            var today = time.getFullYear() + "-" + (month) + "-" + (day);
+                            $('#data').val(today);
+                        }
+                        if (month >= 10 && day < 10) {
+                            var today = time.getFullYear() + "-" + (month) + "-" + ('0' + day);
+                            $('#data').val(today);
+                        }
+                        if (month < 10 && day >= 10) {
+                            var today = time.getFullYear() + "-" + ('0' + month) + "-" + (day);
+                            $('#data').val(today);
+                        }
+                        _template1 = buildJson();
+                        var s = ("svr=webadmin_00003" + "&fsession=" + fsession);
+                        var URL = "/webadmin/?" + s;
+                        var form = new FormData();
+                        form.append("data", (JSON.stringify(_template1)));
+                        var arr = [];
+                        var arrs = [];
+                        $.ajax({
+                            type: "post", //请求的方式，也有get请求
+                            url: URL, //请求地址，后台提供的,这里我在本地自己建立了个json的文件做例子
+                            data: form,//data是传给后台的字段，后台需要哪些就传入哪些
+                            dataType: "json", //json格式，后台返回的数据为json格式的。
+                            cache: false,
+                            processData: false,
+                            contentType: false,
+                            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                                alert(XMLHttpRequest.status);
+                            },
+                            success: function (result) {
+                                var dataObj = result;
+                                if (dataObj.ret.ls.length == 0) {
+                                    alert('当天没有服务日志');
+                                }
+                                var con = '';
+                                document.getElementById('code').value = dataObj.ret.content;
+                                $.each(dataObj.ret.ls, function (indexs, item) {
+                                    arr.push(item);
+                                });
+                                for (var i = arr.length - 1; i >= 0; i--) {
+                                    con += "<li>"
+                                        + "<span class='log--icon__time' id=" + arr[i].nm + ">" + arr[i].m + "</span>"
+                                        + "</li>";
+                                }
+                                $("#ul").html(con);
+                                $("span").click(function () {
+                                    logs(this.id);
+                                });
+                            }
+                        });
+                        function buildJson() {
+                            var std = JSON.stringify({});
+                            var stdTemplate = JSON.parse(std);
+                            stdTemplate.y = year.toString();
+                            stdTemplate.m = month.toString();
+                            stdTemplate.d = day.toString();
+                            stdTemplate.svr = input;
+                            return stdTemplate;
+                        }
+                    }
                 });
             }
         });
-        function buildJson() {
-            var std = JSON.stringify({});
-            var stdTemplate = JSON.parse(std);
-            stdTemplate.y = year.toString();
-            stdTemplate.m = month.toString();
-            stdTemplate.d = day.toString();
-            stdTemplate.svr = input;
-            return stdTemplate;
-        }
     })
 });
 
